4c34b1489c88df6a51ba1591d7becb99b44e6e76
WetEgg <jackeccleston04@gmail.com>
2024-11-07 08:30:59 +0000

    Create C# Console App

---
+++ codhashfinder/Program.cs
@@ -0,0 +1,9 @@
+﻿class CODHashFinder
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(@"Welcome to the COD Hash Finder! Please select an operation:"
+        
+        );
+    }
+}
