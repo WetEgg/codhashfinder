a58fee0c525e940c19fa77d5738945475e9834ed
WetEgg <jackeccleston04@gmail.com>
2024-11-10 16:13:32 +0000

    Added material functions

--- codhashfinder/Program.cs
+++ codhashfinder/Program.cs
@@ -39,6 +39,14 @@ class CODHashFinder
 
             4 - Materials
 
+                41 - MW2/3 Materials (IW9/JUP)
+
+                42 - BO6 Materials (T10)
+
+                43 - BO6 Model Materials (T10)
+
+                44 - BO6 Blueprint Materials (T10)
+
             5 - Models
 
             6 - Sounds
@@ -47,9 +55,23 @@ class CODHashFinder
 
                 71 - Unhash Soundbanks
 
+            8 - Old Hashes
+
+                81 - Old Animations
+
+                82 - Old Images
+
+                83 - Old Materials
+
+                84 - Old Models
+
+                85 - Old Sounds
+
             9 - Miscellanious
 
                 91 - Asset Logs
+
+                98 - Hasher
 
                 99 - Toggle Debug
 
@@ -95,15 +117,75 @@ class CODHashFinder
 
                             break;
                         }
+                        case "41":
+                        {
+                            JUPMaterials();
+                            
+                            break;
+                        }
+                        case "42":
+                        {
+                            T10Materials();
+                            
+                            break;
+                        }
+                        case "43":
+                        {
+                            T10ModelMaterials();
+                            
+                            break;
+                        }
+                        case "44":
+                        {
+                            T10BPMaterials();
+                            
+                            break;
+                        }
                         case "71":
                         {
                             Soundbanks();
 
                             break;
                         }
+                        case "81":
+                        {
+                            OldAnims();
+
+                            break;
+                        }
+                        case "82":
+                        {
+                            OldImages();
+
+                            break;
+                        }
+                        case "83":
+                        {
+                            OldMaterials();
+
+                            break;
+                        }
+                        case "84":
+                        {
+                            OldModels();
+
+                            break;
+                        }
+                        case "85":
+                        {
+                            OldSounds();
+
+                            break;
+                        }
                         case "91":
                         {
                             AssetLogs();
+
+                            break;
+                        }
+                        case "98":
+                        {
+                            Hasher();
 
                             break;
                         }
@@ -344,9 +426,11 @@ class CODHashFinder
 
         Console.WriteLine("Generating Image names from Materials:\n");
 
-        string[] ImageAssetLog = File.ReadAllLines(@"Files\Asset Logs\" + game + "AnimAssetLog.txt");
+        string[] ImageAssetLog = File.ReadAllLines(@"Files\Asset Logs\" + game + "ImageAssetLog.txt");
         string[] MaterialNames = File.ReadAllLines(@"Files\Images\MaterialNames.txt");
         string[] TextureTypes = File.ReadAllLines(@"Files\Images\TextureNames.txt");
+
+        using StreamWriter GeneratedHashesImages = new StreamWriter(@"Files\GeneratedHashesImages.txt");
 
         string[] foundHashes = [];
 
@@ -373,6 +457,181 @@ class CODHashFinder
                 });
             });
         });
+
+        foreach(string foundHash in foundHashes)
+        {
+            GeneratedHashesImages.WriteLine(foundHash);
+        }
+    }
+
+    static void JUPMaterials()
+    {
+        Console.WriteLine("Generating JUP Material Names:\n");
+
+        string[] JUPMaterialAssetLog = File.ReadAllLines(@"Files\Asset Logs\JUPMaterialAssetLog.txt");
+        string[] MaterialKeywords = File.ReadAllLines(@"Files\Materials\Keywords.txt");
+        string[] JUPNumbers = File.ReadAllLines(@"Files\Materials\JUPNumbers.txt");
+        string[] JUPWeaponNames = File.ReadAllLines(@"Files\Shared\JupWeaponNames.txt");
+
+        using StreamWriter GeneratedHashesMaterials = new StreamWriter(@"Files\GeneratedHashesMaterials.txt");
+
+        string[] foundHashes = [];
+
+        Parallel.ForEach(JUPNumbers, JUPNumber =>
+        {
+            Parallel.ForEach(MaterialKeywords, Keyword =>
+            {
+                Parallel.ForEach(JUPWeaponNames, JUPWeaponName =>
+                {
+                    string stringedName = "m/mtl_jup_" + JUPWeaponName + "_" + Keyword + JUPNumber;
+                    string generatedHash = CalcHash(stringedName);
+
+                    if(Globals.DebugToggle)
+                    {
+                        Console.WriteLine(stringedName);
+                    }
+
+                    Parallel.ForEach(JUPMaterialAssetLog, hashedAsset => 
+                    {
+                        if(generatedHash == hashedAsset)
+                        {
+                            string fullHash = generatedHash + "," + stringedName;
+                            foundHashes = foundHashes.Append(fullHash).ToArray();
+                            Console.WriteLine(fullHash);
+                        }
+                    });
+                });
+            });
+        });
+
+        foreach(string foundHash in foundHashes)
+        {
+            GeneratedHashesMaterials.WriteLine(foundHash);
+        }
+    }
+
+    static void T10Materials()
+    {
+        Console.WriteLine("Generating T10 Material Names:\n");
+        
+        string[] T10MaterialAssetLog = File.ReadAllLines(@"Files\Asset Logs\T10MaterialAssetLog.txt");
+        string[] MaterialKeywords = File.ReadAllLines(@"Files\Materials\Keywords.txt");
+        string[] T10WeaponNames = File.ReadAllLines(@"Files\Shared\T10WeaponNames.txt");
+
+        using StreamWriter GeneratedHashesMaterials = new StreamWriter(@"Files\GeneratedHashesMaterials.txt");
+
+        string[] foundHashes = [];
+
+        Parallel.ForEach(MaterialKeywords, Keyword =>
+        {
+            Parallel.ForEach(T10WeaponNames, T10WeaponName =>
+            {
+                string stringedName = "m/mtl_t10_" + T10WeaponName + "_" + Keyword;
+                string generatedHash = CalcHash(stringedName);
+
+                if(Globals.DebugToggle)
+                {
+                    Console.WriteLine(stringedName);
+                }
+
+                Parallel.ForEach(T10MaterialAssetLog, hashedAsset => 
+                {
+                    if(generatedHash == hashedAsset)
+                    {
+                        string fullHash = generatedHash + "," + stringedName;
+                        foundHashes = foundHashes.Append(fullHash).ToArray();
+                        Console.WriteLine(fullHash);
+                    }
+                });
+            });
+        });
+
+        foreach(string foundHash in foundHashes)
+        {
+            GeneratedHashesMaterials.WriteLine(foundHash);
+        }
+    }
+
+    static void T10ModelMaterials()
+    {
+        Console.WriteLine("Generating T10 Material Names From Models:\n");
+        
+        string[] T10MaterialAssetLog = File.ReadAllLines(@"Files\Asset Logs\T10MaterialAssetLog.txt");
+        string[] T10ModelNames = File.ReadAllLines(@"Files\Materials\T10WeaponModelNames.txt");
+
+        using StreamWriter GeneratedHashesMaterials = new StreamWriter(@"Files\GeneratedHashesMaterials.txt");
+
+        string[] foundHashes = [];
+
+        Parallel.ForEach(T10ModelNames, T10ModelName =>
+        {
+            string stringedName = "m/mtl_" + T10ModelName;
+            string generatedHash = CalcHash(stringedName);
+
+            if(Globals.DebugToggle)
+            {
+                Console.WriteLine(stringedName);
+            }
+
+            Parallel.ForEach(T10MaterialAssetLog, hashedAsset => 
+            {
+                if(generatedHash == hashedAsset)
+                {
+                    string fullHash = generatedHash + "," + stringedName;
+                    foundHashes = foundHashes.Append(fullHash).ToArray();
+                    Console.WriteLine(fullHash);
+                }
+            });
+        });
+
+        foreach(string foundHash in foundHashes)
+        {
+            GeneratedHashesMaterials.WriteLine(foundHash);
+        }
+    }
+
+    static void T10BPMaterials()
+    {
+        string game = PickGame();
+
+        Console.WriteLine("Generating Blueprint Material Names:\n");
+
+        string[] T10MaterialAssetLog = File.ReadAllLines(@"Files\Asset Logs\T10MaterialAssetLog.txt");
+        string[] T10BPNames = File.ReadAllLines(@"Files\Materials\T10BlueprintNames.txt");
+        string[] MaterialNames = File.ReadAllLines(@"Files\Images\MaterialNames.txt");
+
+        using StreamWriter GeneratedHashesMaterials = new StreamWriter(@"Files\GeneratedHashesMaterials.txt");
+
+        string[] foundHashes = [];
+
+        Parallel.ForEach(MaterialNames, MaterialName =>
+        {
+            Parallel.ForEach(T10BPNames, BPName =>
+            {
+                string stringedName = "m/" + MaterialName + BPName;
+                string generatedHash = CalcHash(stringedName);
+
+                if(Globals.DebugToggle)
+                {
+                    Console.WriteLine(stringedName);
+                }
+
+                Parallel.ForEach(T10MaterialAssetLog, hashedAsset => 
+                {
+                    if(generatedHash == hashedAsset)
+                    {
+                        string fullHash = generatedHash + "," + stringedName;
+                        foundHashes = foundHashes.Append(fullHash).ToArray();
+                        Console.WriteLine(fullHash);
+                    }
+                });
+            });
+        });
+
+        foreach(string foundHash in foundHashes)
+        {
+            GeneratedHashesMaterials.WriteLine(foundHash);
+        }
     }
 
     static void Soundbanks()
@@ -454,6 +713,142 @@ class CODHashFinder
                 }
             }
         });
+    }
+
+    static void OldAnims()
+    {
+        Console.WriteLine("Generating Anim names from Materials:\n");
+
+        if(!File.Exists(@"Files\GeneratedHashesAnims.txt"))
+        {
+            var file = File.Create(@"Files\GeneratedHashesAnims.txt");
+            file.Close();
+        }
+
+        using StreamWriter GeneratedHashesAnims = new StreamWriter(@"Files\GeneratedHashesAnims.txt");
+
+        string[] JupAnimAssetLog = File.ReadAllLines(@"Files\Asset Logs\JUPAnimAssetLog.txt");
+        string[] T10AnimAssetLog = File.ReadAllLines(@"Files\Asset Logs\T10AnimAssetLog.txt");
+
+        string[] OldAnims = File.ReadAllLines(@"Files\Old Hashes\OldAnims.txt");
+
+        foreach(string hashedAnim in JupAnimAssetLog)
+        {
+            foreach(string animName in OldAnims)
+            {
+                if(CalcHash(animName) == hashedAnim)
+                {
+                    Console.WriteLine(animName + " | " + hashedAnim);
+                    GeneratedHashesAnims.WriteLine(hashedAnim + "," + animName);
+                }
+            }
+        }
+
+        foreach(string hashedAnim in T10AnimAssetLog)
+        {
+            foreach(string animName in OldAnims)
+            {
+                if(CalcHash(animName) == hashedAnim)
+                {
+                    Console.WriteLine(animName + " | " + hashedAnim);
+                    GeneratedHashesAnims.WriteLine(hashedAnim + "," + animName);
+                }
+            }
+        }
+    }
+
+    static void OldImages()
+    {
+        Console.WriteLine("Generating Image names from Materials:\n");
+
+        if(!File.Exists(@"Files\GeneratedHashesImages.txt"))
+        {
+            var file = File.Create(@"Files\GeneratedHashesImages.txt");
+            file.Close();
+        }
+
+        using StreamWriter GeneratedHashesImages = new StreamWriter(@"Files\GeneratedHashesImages.txt");
+
+        string[] JupImageAssetLog = File.ReadAllLines(@"Files\Asset Logs\JUPImageAssetLog.txt");
+        string[] T10ImageAssetLog = File.ReadAllLines(@"Files\Asset Logs\T10ImageAssetLog.txt");
+
+        string[] OldImages = File.ReadAllLines(@"Files\Old Hashes\OldImages.txt");
+
+        foreach(string hashedImage in JupImageAssetLog)
+        {
+            foreach(string imageName in OldImages)
+            {
+                if(CalcHash(imageName) == hashedImage)
+                {
+                    Console.WriteLine(imageName + " | " + hashedImage);
+                    GeneratedHashesImages.WriteLine(hashedImage + "," + imageName);
+                }
+            }
+        }
+
+        foreach(string hashedImage in T10ImageAssetLog)
+        {
+            foreach(string imageName in OldImages)
+            {
+                if(CalcHash(imageName) == hashedImage)
+                {
+                    Console.WriteLine(imageName + " | " + hashedImage);
+                    GeneratedHashesImages.WriteLine(hashedImage + "," + imageName);
+                }
+            }
+        }
+    }
+
+    static void OldMaterials()
+    {
+        Console.WriteLine("Generating material names from Materials:\n");
+
+        if(!File.Exists(@"Files\GeneratedHashesMaterials.txt"))
+        {
+            var file = File.Create(@"Files\GeneratedHashesMaterials.txt");
+            file.Close();
+        }
+
+        using StreamWriter GeneratedHashesMaterials = new StreamWriter(@"Files\GeneratedHashesMaterials.txt");
+
+        string[] JupMaterialAssetLog = File.ReadAllLines(@"Files\Asset Logs\JUPMaterialAssetLog.txt");
+        string[] T10MaterialAssetLog = File.ReadAllLines(@"Files\Asset Logs\T10MaterialAssetLog.txt");
+
+        string[] OldMaterials = File.ReadAllLines(@"Files\Old Hashes\Oldmaterials.txt");
+
+        foreach(string hashedMaterial in JupMaterialAssetLog)
+        {
+            foreach(string materialName in OldMaterials)
+            {
+                if(CalcHash(materialName) == hashedMaterial)
+                {
+                    Console.WriteLine(materialName + " | " + hashedMaterial);
+                    GeneratedHashesMaterials.WriteLine(hashedMaterial + "," + materialName);
+                }
+            }
+        }
+
+        foreach(string hashedMaterial in T10MaterialAssetLog)
+        {
+            foreach(string materialName in OldMaterials)
+            {
+                if(CalcHash(materialName) == hashedMaterial)
+                {
+                    Console.WriteLine(materialName + " | " + hashedMaterial);
+                    GeneratedHashesMaterials.WriteLine(hashedMaterial + "," + materialName);
+                }
+            }
+        }
+    }
+
+    static void OldModels()
+    {
+
+    }
+
+    static void OldSounds()
+    {
+
     }
 
     static void AssetLogs()
@@ -577,6 +972,25 @@ class CODHashFinder
 
                     break;
                 }
+            }
+        }
+    }
+
+    static void Hasher()
+    {
+        for(;;)
+        {
+            Console.WriteLine("Enter an asset name to hash, type 'Quit' to cancel:\n");
+
+            string? userResponse = Console.ReadLine();
+
+            if(userResponse == "Quit" || userResponse == "quit")
+            {
+                break;
+            }
+            else if(userResponse != null)
+            {
+                Console.WriteLine(userResponse + " | " + CalcHash(userResponse));
             }
         }
     }
